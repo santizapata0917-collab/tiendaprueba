88feea9034048bb9aedc8e1d0f1766070a8e3b9d
Mañana <santizapata0917@gmail.com>
2026-06-25 06:34:21 -0500

    Se eliminan archivos duplicados del backend

--- config/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose.connect(process.env.MONGO_URI)
-.then(() => {
-    console.log("MongoDB conectado");
-})
-.catch((error) => {
-    console.log(error);
-});
